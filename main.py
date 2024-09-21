4e4f913477799089373c3a27c2da0c84c1073eab
Redume <danilivasenko09@gmail.com>
2024-09-21 20:42:44 +0300

    git init

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+from fastapi import FastAPI
+
+from src.routes import index
+
+app = FastAPI()
+app.include_router(index.router)
+app.mount('/static/', StaticFiles(directory='./src/web/static/'))
+
+
+@app.exception_handler(404)
+async def not_found(req, __):
+    return FileResponse('./src/web/errors/404.html')
