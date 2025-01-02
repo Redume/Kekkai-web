7795d7de049af4bdd988336d9ae3cc79cb6c2964
Redume <danilivasenko09@gmail.com>
2025-01-02 15:52:47 +0300

    chore: deleted fastapi backend

--- src/routes/index.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
-router = APIRouter()
-
-
-@router.get('/')
-async def main():
-    return FileResponse('./src/web/page/index.html')
